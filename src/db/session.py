a1efaf62ec5a6d65fa3442a85eb907be340850e7
quilix11 <nazarus8095@gmail.com>
2026-05-05 08:20:43 +0300

    create session maker and config

---
+++ src/db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from src.core.config import settings
+
+
+engine = create_async_engine(settings.DATABASE_URL)
+
+async_session_factory = async_sessionmaker(
+    bind=engine, 
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+async def get_async_session():
+    async with async_session_factory() as session:
+        yield session
+
